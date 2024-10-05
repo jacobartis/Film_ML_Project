f4acf368b904c7cff55038f9a6c0bacf0c10c508
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-10-05 19:39:22 +0100

    Created test graph, loads data set and displays rating vs domestic gross.

---
+++ FilmML.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data_set = pd.read_csv("movie_statistic_dataset.csv")
+print(data_set.keys())
+
+graph = plt.plot(data_set["movie_averageRating"],data_set["Domestic gross $"], '.', alpha=.5, label='cars')
+
+plt.show()
