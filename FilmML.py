a7694de25d13d6a4ae781e87a06f1d15ca51d3d6
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-10-18 22:04:55 +0100

    Using sklearn instead of Pytorch to start. Created test for best KNN k value.

--- FilmML.py
+++ FilmML.py
@@ -1,11 +1,35 @@
 import numpy as np
 import pandas as pd
-import pytorch as pt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
 from matplotlib import pyplot as plt
 
-data_set = pd.read_csv("movie_statistic_dataset.csv")
-print(data_set.keys())
+raw_data = pd.read_csv("movie_statistic_dataset.csv")
+print(raw_data.keys())
 
-graph = plt.plot(data_set["movie_averageRating"],data_set["Domestic gross $"], '.', alpha=.5, label='cars')
+data_set = []
+labels = []
 
+for x in range(len(raw_data.values)):
+    entry = [int(raw_data['Production budget $'][x]),int(raw_data['Worldwide gross $'][x]),float(raw_data['runtime_minutes'][x])]
+    data_set.append(entry)
+    labels.append(raw_data['movie_averageRating'][x])
+x_training, x_test, y_training, y_test = train_test_split(data_set,labels,random_state=0)
+
+scores = []
+best_k = 0
+
+for k in range(20,250):
+    knn = KNeighborsRegressor(n_neighbors=k)
+    knn.fit(x_training,y_training)
+    score = knn.score(x_test,y_test)
+    if score>=0:
+        scores.append(knn.score(x_test,y_test))
+    if score>=max(scores):
+        best_k = k
+    print(k)
+
+
+plt.plot(range(20,250),scores,"*")
 plt.show()
+print("best k: ", best_k)
